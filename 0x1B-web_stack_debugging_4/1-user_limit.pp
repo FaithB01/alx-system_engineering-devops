031f532ba205cf1fa90c335b5fecb6f9ecba7823
Faith Biketi <78912860+FaithB01@users.noreply.github.com>
2022-12-09 10:34:40 +0300

    user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,11 @@
-# Changes the OS configuration so that it is possible to login with the holberton user and open a file without any error message
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+# set limits on user.
+
+file { '/etc/security/limits.conf':
+  ensure => present,
+} -> exec { 'Limit HARD':
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile unlimited/" /etc/security/limits.conf',
+  path    => '/bin',
+} -> exec { 'Limit SOFT':
+  command => 'sed -i "s/holberton soft nofile 4/holberton hard nofile 1000/" /etc/security/limits.conf',
+  path    => '/bin',
+}
